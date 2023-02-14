9239f26dcdd699bd5174ffbb78e89f281507fc1a
Nanda kumar <110214837+Nandakumar7328@users.noreply.github.com>
2023-02-14 18:14:38 +0530

    add nav fixed score error simplyfy the query

--- production/views.py
+++ production/views.py
@@ -3,17 +3,11 @@ from django.http import HttpResponse
 from datetime import datetime
 from .models import Data
 from django.shortcuts import redirect
+from django.core.exceptions import ObjectDoesNotExist
+
 
 
 def home(request):
-    myData = Data.objects.all()
-    if (myData != ''):
-        return render(request,'index.html',{'datas':myData})
-    else:
-        return render(request,'index.html')
-
-
-def addData(request):
     if request.method == 'POST':
         estimated = int(request.POST["one"])
         achived = int(request.POST["two"])
@@ -25,40 +19,70 @@ def addData(request):
         dt_string = now.strftime('%Y-%m-%d')
         time_str = now.strftime( '%H:%M:%S')
         
-        obj = Data()
-        obj.Estimated_target = estimated
-        obj.Achieved_target = achived 
-        obj.Score = result
-        obj.Bonus = bonus
-        obj.Datetime = dt_string
-        obj.Time = time_str
-        obj.save()
-        myData = Data.objects.all()
+        Data.objects.create(
+            Estimated_target = estimated,
+            Achieved_target = achived,
+            Score = result,
+            Bonus = bonus,
+            Datetime = dt_string,
+            Time = time_str
+        )
         return redirect('home')
-    return render(request,'index.html')
+    
+    
+    data = Data.objects.all() 
+    if (data != ''):
+         return render(request, 'index.html',{'datas':data})
+    else:
+         return render(request, 'index.html')
+     
+# def addData(request):
+#     if request.method == 'POST':
+#         estimated = int(request.POST["one"])
+#         achived = int(request.POST["two"])
+#         result = round((achived/ estimated)* 100)
+#         bonus = 0 
+#         if result > estimated :
+#             bonus = result - estimated 
+#         now = datetime.now()
+#         dt_string = now.strftime('%Y-%m-%d')
+#         time_str = now.strftime( '%H:%M:%S')
+        
+#         Data.objects.create(
+#             Estimated_target = estimated,
+#             Achieved_target = achived,
+#             Score = result,
+#             Bonus = bonus,
+#             Datetime = dt_string,
+#             Time = time_str
+#         )
+#         return redirect('home')
+#     return render(request,'index.html')
 
 def updateData(request,id):
     myData = Data.objects.get(id=id)
     if request.method == "POST":
         estimated = int(request.POST["Estimated"])
         achived = int(request.POST["Achieved"])
-        result = int(request.POST["Score"])
-        bonus = int(request.POST["Bonus"])
+        result = round((achived/ estimated)* 100)
+        bonus = 0 
+        if result > estimated :
+            bonus = result - estimated 
         now = datetime.now()
         dt_string = now.strftime('%Y-%m-%d')
         time_str = now.strftime( '%H:%M:%S')
-        myData.Estimated_target = estimated 
-        myData.Achieved_target = achived 
-        myData.Score = result 
-        myData.Bonus = bonus 
-        myData.Datetime = dt_string 
-        myData.Time = time_str
-        myData.save() 
+        myData = Data.objects.filter(id=id).update(
+            Estimated_target = estimated ,
+            Achieved_target = achived ,
+            Score = result ,
+            Bonus = bonus ,
+            Datetime = dt_string,
+            Time = time_str
+        )
         
         return redirect('home')
         
     return render(request,'update.html',{'datas':myData})
-
 
 def deleteData(request,id):
     myData = Data.objects.get(id=id)
