f19e157f42c59bed4bb1f4caca85edf5151ed5b5
Nanda kumar <110214837+Nandakumar7328@users.noreply.github.com>
2023-02-16 10:07:43 +0530

    error in filter fixed

--- production/views.py
+++ production/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse 
-from datetime import datetime
 from .models import Data
 from django.shortcuts import redirect
 from django.core.exceptions import ObjectDoesNotExist
 from django.db.models import Max,Min
+from datetime import datetime
+from django.utils.datastructures import MultiValueDictKeyError
 
 def home(request):
     if request.method == 'POST':
@@ -35,29 +36,6 @@ def home(request):
          return render(request, 'index.html',{'datas':data})
     else:
          return render(request, 'index.html')
-     
-# def addData(request):
-#     if request.method == 'POST':
-#         estimated = int(request.POST["one"])
-#         achived = int(request.POST["two"])
-#         result = round((achived/ estimated)* 100)
-#         bonus = 0 
-#         if result > estimated :
-#             bonus = result - estimated 
-#         now = datetime.now()
-#         dt_string = now.strftime('%Y-%m-%d')
-#         time_str = now.strftime( '%H:%M:%S')
-        
-#         Data.objects.create(
-#             Estimated_target = estimated,
-#             Achieved_target = achived,
-#             Score = result,
-#             Bonus = bonus,
-#             Datetime = dt_string,
-#             Time = time_str
-#         )
-#         return redirect('home')
-#     return render(request,'index.html')
 
 def updateData(request,id):
     myData = Data.objects.get(id=id)
@@ -89,23 +67,29 @@ def deleteData(request,id):
     myData.delete()
     return redirect('home')
 
+
 def dashboard(request):
     if request.method == 'POST':
-        selected_date_str = request.POST.get('datetime')
-        print(selected_date_str,'selected')
-        selected_date = datetime.strftime(selected_date_str, '%Y-%m-%d').date()
-        print(selected_date)
-        top_score = Data.objects.filter(Datetime=selected_date).aggregate(Max('Score'))['Score__max']
-        low_score =  Data.objects.filter(Datetime=selected_date).aggregate(Min('Score'))['Score__min']
-        all_order_data = Data.objects.order_by('-Score')
-        send_data = {'top':top_score,'low':low_score,'allData':all_order_data}
-        return render(request,'dashboard.html',{'topscore':send_data})
+        try:
+            value = request.POST['datetime']
+            if not value:
+                return redirect('dashboard')
+            print(value, 'selected')
+            date = datetime.strptime(value, '%Y-%m-%d').date()
+            print(date)
+            top_score = Data.objects.filter(Datetime=date).aggregate(Max('Score'))['Score__max']
+            low_score = Data.objects.filter(Datetime=date).aggregate(Min('Score'))['Score__min']
+            all_order_data = Data.objects.order_by('-Score')
+            send_data = {'top':top_score,'low':low_score,'allData':all_order_data}
+            return render(request,'dashboard.html',{'topscore':send_data})
+        
+        except MultiValueDictKeyError:
+            pass
     else:
         top_score = Data.objects.aggregate(Max('Score'))['Score__max']
         low_score = Data.objects.aggregate(Min('Score'))['Score__min']
         all_order_data = Data.objects.order_by('-Score')
         chart_data = Data.objects.all()
-        import datetime
         chart_series = {
             'name': 'Scores',
             'data': [{'x': item.Datetime.strftime("%Y-%m-%d"), 'y': item.Score} for item in chart_data],
